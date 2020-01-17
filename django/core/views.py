7c9ff2270db31f4b0044e590e2a23745cb974ea5
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2020-01-18 00:41:13 +0100

    Created the application - named 'core'

---
+++ django/core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Movie
+# Create your views here.
+
+
+class MovieList(ListView):
+    model = Movie
+    context_object_name = "movie_list"
+    template_name = "core:MovieList"
+
+
+class MovieDetail(DetailView):
+    model = Movie
+    context_object_name = "movie"
