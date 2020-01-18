fb3bcf2cd22ec264e9a3802feaf3abd1c298dbd8
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2020-01-18 11:41:00 +0100

    Added pagination to movie list template

--- django/core/views.py
+++ django/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 
 from .models import Movie
 # Create your views here.
@@ -9,6 +10,7 @@ class MovieList(ListView):
     model = Movie
     context_object_name = "movie_list"
     template_name = "core:MovieList"
+    paginate_by = 2
 
 
 class MovieDetail(DetailView):
