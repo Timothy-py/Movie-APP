c4766a131554e1e7a97e9582f29316bf9bd86a36
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2020-01-22 23:22:39 +0100

    Added user login and logout functionalities

--- django/user/urls.py
+++ django/user/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import RegisterView
 
@@ -6,4 +7,6 @@ app_name = 'user'
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
 ]
