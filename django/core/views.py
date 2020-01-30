bdb2e3e6ff0657d87dfefedb8cbdd92b91049943
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2020-01-30 13:10:33 +0100

    created create and update vote view

--- django/core/views.py
+++ django/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import (PermissionDenied)
 
 from .models import Movie, Person, Vote
 from .forms import VoteForm
@@ -53,3 +56,52 @@ class MovieDetail(DetailView):
 
 class PersonDetail(DetailView):
     queryset = Person.objects.all_with_prefetch_movies()
+
+
+class CreateVote(LoginRequiredMixin, CreateView):
+    form_class = VoteForm
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['user'] = self.request.user.id
+        initial['movie'] = self.kwargs['movie_id']
+        return initial
+
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+        return reverse(
+            "core:MovieDetail", kwargs={'pk': movie_id}
+        )
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+        movie_detail_url = reverse(
+            "core:MovieDetail", kwargs={'pk': movie_id}
+        )
+        return redirect(to=movie_detail_url)
+
+
+class UpdateVote(LoginRequiredMixin, UpdateView):
+    form_class = VoteForm
+    queryset = Vote.objects.all()
+
+    def get_object(self, queryset=None):
+        vote = super().get_object(queryset)
+        user = self.request.user
+
+        if vote.user != user:
+            raise PermissionDenied('cannot change another user\'s vote')
+        return vote
+
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+        return reverse(
+            'core: MovieDetail', kwargs={'pk': movie_id}
+        )
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+        movie_detail_url = reverse(
+            'core:MovieDetail', kwargs={'pk': movie_id}
+        )
+        return redirect(to=movie_detail_url)
